2844329633e0af9cc1182e8ffe402b7ff4f9bad3
Chris Geihsler <chris@geihsler.net>
2011-05-21 10:36:57 -0400

    DSLish logging channel

--- lib/hl7-processor/channels/logging.rb
+++ lib/hl7-processor/channels/logging.rb
@@ -2,14 +2,22 @@ require 'logger'
 
 module HL7Processor
   module Channels
-    class Logging
+    module Logging
 
-      def initialize(filename = "hl7_log.txt")
-        @log = Logger.new(filename)
+      def log_file(filename)
+        @log_file = filename
+      end
+
+      def for_all_messages(&block)
+        @all_messages_block = block
+      end
+
+      def log
+        @log ||= Logger.new(@log_file)
       end
 
       def handle(hl7)
-        @log.info("Received a message: #{hl7}")
+        @all_messages_block.call(hl7)
       end
 
     end
